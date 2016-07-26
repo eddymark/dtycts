7f0d0ef1daad48e7dcbbe3f6d7a475940c78c3ca
eddy <eddyv5413@gmail.com>
2016-07-25 21:25:44 -0400

    fixed the Navbar

--- shared/layout.php
+++ shared/layout.php
@@ -1,14 +1,14 @@
 <div class= "left">
     <ul class="nav nav-pills nav-stacked">
 
-        <li ><a href="http://www.inft.me/~eddy/home">Home</a></li>
-        <li><a href="http://www.inft.me/~eddy/hosting">Hosting a DTYCTS Event </a></li>
-        <li><a href="http://www.inft.me/~eddyt/hemes">Themes</a></li>
+        <li ><a href="http://www.inft.me/~eddy/index.php">Home</a></li>
+        <li><a href="http://www.inft.me/~eddy/dtycts">Hosting a DTYCTS Event </a></li>
+        <li><a href="http://www.inft.me/~eddy/themes">Themes</a></li>
         <li><a href="http://www.inft.me/~eddy/media">Media</a></li>
         <li><a href="http://www.inft.me/~eddy/registration">Registration</a></li>
         <li><a href="http://www.inft.me/~eddy/history">History</a></li>
         <li><a href="http://www.inft.me/~eddy/psa">Psa</a></li>
-        <li><a href="http://www.inft.me/~eddy/dtycts/quotes">Quotes and Supports </a></li>
+        <li><a href="http://www.inft.me/~eddy/quotesandsupport">Quotes and Supports </a></li>
         <li><a href="http://www.inft.me/~eddy/aboutus">About Us</a></li>
         <li><a href="http://www.inft.me/~eddy/contactus">Contact us </a></li>
     </ul>
